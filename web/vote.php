9c4d6ab72aee011a61597e5fbf7539d7726b9ae1
jb567 <jacobbeal@hotmail.co.uk>
2014-01-09 09:32:22 +1300

    testing UI

--- web/vote.php
+++ web/vote.php
@@ -6,9 +6,17 @@
 	</head>
 	<body>
 		<div>
-			<?php 
-				echo "PHP is online";
-			?>
+			<table>
+				<tr>
+					<td>
+						<img src="http://www.impawards.com/intl/uk/2013/posters/worlds_end_ver5_xlg.jpg" alt="The World's End Movie Poster" />
+						<a href='http://www.imdb.com/title/tt1213663/'>
+							<img src="http://ia.media-imdb.com/images/M/MV5BMjAzOTcxMDA2Nl5BMl5BcG5nXkFtZTcwMTc1MzIzOA@@._V1_.png" alt="IMDb Database entry" />
+						</a>
+						
+					</td>
+				</tr>
+			</table>
 		</div>
 	</body>
 </html>
