f17affe44017740bc662f619661cd7c323f09918
jb567 <jacobbeal@hotmail.co.uk>
2014-01-10 13:46:57 +1300

    test

--- web/vote.php
+++ web/vote.php
@@ -9,14 +9,43 @@
 			<table>
 				<tr>
 					<td>
-						<img src="http://www.impawards.com/intl/uk/2013/posters/worlds_end_ver5_xlg.jpg" alt="The World's End Movie Poster" />
-						<a href='http://www.imdb.com/title/tt1213663/'>
-							<img src="http://ia.media-imdb.com/images/M/MV5BMjAzOTcxMDA2Nl5BMl5BcG5nXkFtZTcwMTc1MzIzOA@@._V1_.png" alt="IMDb Database entry" />
-						</a>
-						
+						<img src="img/worldsend.jpg" alt="The World's End Movie Poster" width="327"/>
 					</td>
+					<td>
+						<h1>
+							The World's End
+						</h1>
+						<p>
+							Five friends who reunite in an attempt to top their epic pub crawl from 20 years earlier unwittingly become humankind's only hope for survival.
+								<BR />
+							Director: Edgar Wright
+								<BR />
+							Writers: Simon Pegg, Edgar Wright
+								<BR />
+							Stars: Simon Pegg, Nick Frost, Martin Freeman
+						</p>
+					<td>
+				</tr>
+				<tr>
+					<td>
+						<center>
+						<a href='http://www.imdb.com/title/tt1213663/'>
+							<img src="img/imdb.png" alt="IMDb Database entry for 'The World's End'" width="100px"/>
+						</a> <img src="img/metascore.png" height=100px width=100px />
+					</center>
+					</td>
+					<td>
+						<img src="img/Vote.png" onmouseover="this.src='img/voteh.png'" onmouseout="this.src='img/Vote.png'" />
 				</tr>
 			</table>
+			<hr />
 		</div>
+
+		<?php
+			$title = ("Captain Phillips");
+			$raw = file_get_contents('http://www.omdbapi.com/?t=' . urlencode($title));
+			$jsonArray = json_decode($raw, true);
+			echo "<img src='" . $jsonArray['Poster'] . "' alt='Poster for " . $title . "' width=327px/>";
+	?>
 	</body>
 </html>
